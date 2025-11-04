12f75fd4d239ba753682af542f024da70216cbbe
Kuura Omakone <kuuho24@gmail.com>
2025-11-04 20:39:58 +0200

    ulkoasun pienta kehitysta ja kartan zoomauksen muuttamista sijainnin tarkkuuden perusteella

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let coords = null
 let error = null
-const zoom = 15;
+let zoom = 15;
 //const defaultSrc = "https://www.openstreetmap.org/export/embed.html?bbox=3.4936523437500004%2C59.355596110016315%2C50.73486328125001%2C69.88501003874241&amp;layer=mapnik";
 let src = ""
 let locationTapa = ""
@@ -22,8 +22,19 @@ function locationSuccess(position) {
     if(coords) {
         const lat = coords.lat;
         const lon = coords.lon;
-        const accuracy = coords.acc
-        const delta = 0.002; // ~200 m, tweak as needed for zoom level
+        const accuracy = Math.round(coords.acc)
+        let delta = 0.002; // ~200 m, tweak as needed for zoom level
+        if(accuracy < 20) {
+            locationTapa = "GPS:ään"
+            delta = 0.001;
+            zoom = 16
+        } else if (accuracy < 500) {
+            locationTapa = "WiFi-yhteyksiin"
+        } else { 
+            locationTapa="IP:seen"
+            zoom = 14 
+            delta = 0.004;
+        }
         const lonMin = lon - delta;
         const lonMax = lon + delta;
         const latMin = lat - delta;
@@ -31,16 +42,11 @@ function locationSuccess(position) {
         const bbox = `${lonMin},${latMin},${lonMax},${latMax}`; // Kartan näyttämä neliö
         //const circleParams = `&circle=${lat},${lon},${accuracy}`;
         src = `https://www.openstreetmap.org/export/embed.html?bbox=${encodeURIComponent(bbox)}&layer=mapnik&marker=${encodeURIComponent(lat + ',' + lon)}`;
-        if(accuracy < 20) {
-            locationTapa = "GPS:ään"
-        } else if (accuracy < 500) {
-            locationTapa = "WiFi-yhteyksiin"
-        } else { locationTapa="IP:seen" }
         $("#kartta").attr("src", src);
         $("#isokarttalinkki").text("Näytä isommalla kartalla");
         $("#isokarttalinkki").attr("href", `https://www.openstreetmap.org/?mlat=${coords.lat}&mlon=${coords.lon}#map=${zoom}/${coords.lat}/${coords.lon}`);
         $("#locationteksti").text(`Leveysaste: ${coords.lat},  pituusaste: ${coords.lon}`);
-        $("#tapateksti").text(`Tarkkuus: ${coords.acc} m, sen perusteella sijainti perustuu ${locationTapa}`);
+        $("#tapateksti").text(`Tarkkuus: ${accuracy} metriä, tarkkuuden perusteella sijainti perustuu ${locationTapa}`);
     }
 }
 function locationError() {
